832d495d9dc04a666d74d03ab194cf239304c6b1
Shreyam <shreyam601@gmail.com>
2024-10-28 21:58:46 +0530

    first commit

---
+++ src/main/java/com/example/CodeChef/CodeChefApplication.java
@@ -0,0 +1,13 @@
+package com.example.CodeChef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeChefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeChefApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CodeChef/CodeChefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CodeChef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeChefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
